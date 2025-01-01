d44bb21c224dfd123857b7f87615972f154ad34b
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-01-01 12:25:05 +0530

    Dynamic React Routing

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -15,6 +15,8 @@ var RestaurantMenu = () => {
         fetchRestaurantMenu();
     },[])
 
+    console.log('Restaurant Menu Called')
+
 
     var fetchRestaurantMenu = async() => {
         var response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.627597&lng=73.74549&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`);
@@ -22,23 +24,35 @@ var RestaurantMenu = () => {
 
         var data = await response.json();
 
-        console.log(data.data.cards);
+        // console.log(data.data.cards);
+        console.log(data.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards);
 
-        setResMenu(data.data.cards);            
+        // setResMenu(data.data.cards); 
+        setResMenu(data.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards);
+
 
         } 
     
 
     return (
         <div className="res-menu-container">
-            {resMenu.length == 0 ? <h3>Loading...</h3>: <h2>{resMenu[2].card.card.info.name}</h2> } 
+        {console.log('Restaurant menu outside')}
+            {/* {resMenu.length == 0 ? <h3>Loading...</h3>: <h2>{resMenu[2].card.card.info.name}</h2> } 
             {resMenu.length == 0 ? <h3>Loading...</h3> : <h3>{resMenu[2].card.card.info.slugs.restaurant}</h3>}
             {resMenu.length == 0 ? <h3>Loading...</h3> : <h4>{resMenu[2].card.card.info.slugs.city}</h4>}
             {resMenu.length == 0 ? <h3>Loading...</h3> : <ul>
                 <li>{resMenu[2].card.card.info.cuisines.join(", ")}</li>
-            </ul>}
+            </ul>} */}
+            {
+                resMenu.length === 0 ? <h3>Loading...</h3> :
+                resMenu.map((menu) => {
+                    {console.log(menu.card.info.name)}
+                    return <h3 key={menu.card.info.id}>{menu.card.info.name}</h3>
+                })
+            }
         </div>
     );
+    
 }
 
 
